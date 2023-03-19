3a81e172014e363264fffc99b3823b6f1e959171
Kenson Johnson <kenson.johnson@outlook.com>
2023-03-19 18:04:42 -0500

    Adding comments

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import expressLayouts from "express-ejs-layouts";
+import { config } from "dotenv";
+
+// initialize Express app
 const app = express();
-const port = 3000;
+
+// Setup environment variables
+config();
+const PORT = process.env.PORT || 3000;
+
+// ---------------- Configure Express app ---------------- //
 app.use(express.json());
 app.use(express.static("public"));
+
+// ---------------- Set View Engine ---------------- //
 app.set("views", "src/views");
 app.set("view engine", "ejs");
-app.use(expressLayouts);
+app.use(expressLayouts); // express-ejs-layouts
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.render("index");
 });
 
-app.listen(port, () => {
-  return console.log(`http://localhost:${port}`);
+app.listen(PORT, () => {
+  return console.log(`http://localhost:${PORT}`);
 });

2d3c2ff18ce394b66054bfba8d55b79abaf28646
Kenson Johnson <kenson.johnson@outlook.com>
2023-03-20 10:06:01 -0500

    Handle connection to MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import expressLayouts from "express-ejs-layouts";
 import { config } from "dotenv";
+import { connect } from "mongoose";
 
 // initialize Express app
 const app = express();
@@ -12,6 +13,20 @@ const PORT = process.env.PORT || 3000;
 // ---------------- Configure Express app ---------------- //
 app.use(express.json());
 app.use(express.static("public"));
+
+// ---------------- Connect to Database ---------------- //
+const databaseURI = process.env.MONGO_URI;
+if (databaseURI) {
+  connect(databaseURI)
+    .then((result) => console.log("\x1b[33mConnected to DB\x1b[39m"))
+    .catch((error) => {
+      console.error(
+        "\x1b[33mMongoose exited with Error:\n\x1b[39m",
+        error.message
+      );
+      console.error("\x1b[33mIs your MongoDB instance running?\x1b[39m");
+    });
+}
 
 // ---------------- Set View Engine ---------------- //
 app.set("views", "src/views");
